87b4271a265a799cadfc462446571dd43ffecc38
shamimkhan92 <shamimkhan832892@gmail.com>
2024-03-13 22:38:34 +0600

    mogo

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+
+$(".counter").countMe(40,65);
+$(".counte").countMe(40,65);
+$(".count").countMe(40,65);
+$(".coun").countMe(40,65);
+$(".cou").countMe(40,65);
+
+
+
+$('.typed-slick').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	nextArrow:".next",
+	prevArrow:".prev"
+  });
+
+
+
+lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true
+  })
